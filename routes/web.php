9b52f9d14d4c79d715f9989631367c77f1fd6060
KIM CHAN HO <kaelch@naver.com>
2022-01-12 23:19:11 +0900

    react tailwindcss 세팅 및 기본 로그인 폼 작성

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/login', function () {
+    return view('login');
+});
 
 Route::get('/', function () {
     return view('welcome');
