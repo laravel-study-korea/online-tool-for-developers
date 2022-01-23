daaca3fd59cc64db10417c0e673e4d27eabdcb80
ithank <30787504+kaelch@users.noreply.github.com>
2022-01-24 00:19:09 +0900

    front/react (#27)
    * react tailwindcss 세팅 및 기본 로그인 폼 작성
    
    * 메인페이지 및 로그인 페이지 리액트 라우터 연결

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app"></div>
+<script src="{{asset('js/app.js')}}" defer></script>
+</body>
+</html>
